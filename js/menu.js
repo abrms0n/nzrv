ffbaa462c1cc839342d9b7f1e951c1c8371ccda6
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-20 12:01:38 +0300

    menu test

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$('#menu-opener').click(function(){
+    $('.header-mobile__darken').css('display','block');
+    $('.header-mobile__menu').css('left','0');
+})
+
+$('.header-mobile__darken').click(function(){
+    $('.header-mobile__darken').css('display','none');
+    $('.header-mobile__menu').css('left','-360px');
+})
+

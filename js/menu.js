c527661d5137a53a181864e22b6f87251988b058
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-20 18:01:39 +0300

    fixed-body

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,12 @@
 $('#menu-opener').click(function(){
     $('.header-mobile__darken').css('display','block');
     $('.header-mobile__menu').css('left','-16px');
+    $('.body').css('position','fixed');
 })
 
 $('.header-mobile__darken').click(function(){
     $('.header-mobile__darken').css('display','none');
     $('.header-mobile__menu').css('left','-360px');
+    $('.body').css('position','static');
 })
 

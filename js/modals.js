20951cfcbdd1ff1d617abfc9ba84cedf313e7edb
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-24 18:55:46 +0300

    catalog-link-fix

---
+++ js/modals.js
@@ -0,0 +1,13 @@
+function showModal(elId) {
+    document.getElementById(elId).style.display = 'block';
+};
+
+function hideModal(elId) {
+    document.getElementById(elId).style.display = 'none';
+};
+
+
+
+
+
+

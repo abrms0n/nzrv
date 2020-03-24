eacbad13d4f6a1243446dba7fce3f0b4e63903f3
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-24 17:41:14 +0300

    footer-fixed-done

--- js/back-to-top-show.js
+++ js/back-to-top-show.js
@@ -1,15 +1,13 @@
 $(window).scroll(function(){
-if($(window).scrollTop()>600){
-$('.back-to-top').fadeIn()
-}
-
+    if($(window).scrollTop()>600){
+        $('.back-to-top').fadeIn()
+    }
 })
 
 
 
 $(window).scroll(function(){
     if($(window).scrollTop()>700){
-    $('.contact-us').fadeIn()
+        $('.contact-us').fadeIn()
     }
-    
-    })
+})

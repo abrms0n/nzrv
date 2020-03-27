0d51f031dcc098d6b4ee5df373cc1c399ec02806
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-27 16:59:07 +0300

    about-all-done

--- js/showMore.js
+++ js/showMore.js
@@ -3,7 +3,12 @@ if($(window).width() <= '1350') {
     $('.hidden-text').css('display','none');
 }
 
-$('.show-more').click(function(){
-    $('.hidden-text').css('display','block');
-    $('.show-more').css('display','none');
+$('.about-gallery__text-container .show-more').click(function(){
+    $('.about-gallery__text-container .hidden-text').css('display','block');
+    $('.about-gallery__text-container .show-more').css('display','none');
+})
+
+$('.best-brands__text-container .show-more').click(function(){
+    $('.best-brands__text-container .hidden-text').css('display','block');
+    $('.best-brands__text-container .show-more').css('display','none');
 })

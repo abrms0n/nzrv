4ee338b08e5c35522c7a06dcf53bc9f0f1f8cbde
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-27 13:58:21 +0300

    about-intro-done

---
+++ js/showMore.js
@@ -0,0 +1,8 @@
+'use strict'
+if($(window).width() <= '1350') {
+    $('.hidden-text').css('display','none');
+}
+
+$('.show-more').click(function(){
+    $('.hidden-text').css('display','block');
+})

1f88853f89329a75c8d74aced0840ab999c34235
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-30 15:44:14 +0300

    services-almost-done

--- js/solutions-radio.js
+++ js/solutions-radio.js
@@ -3,18 +3,18 @@
 $('.solutions-radio__elem').click(function() {
     event.preventDefault();
 
-    $('.solutions-list__item_active').removeClass('solutions-list__item_active');
+    $('.solutions-item_active').removeClass('solutions-item_active');
     $('.solutions-radio__elem_active').removeClass('solutions-radio__elem_active');
     let tabNo = $(this).attr('data-tab');
-    let tab = $('.solutions-list__item[data-tab="'+ tabNo +'"]');
+    let tab = $('.solutions-item[data-tab="'+ tabNo +'"]');
 
 
     $(this).addClass('solutions-radio__elem_active');
-    tab.addClass('solutions-list__item_active');
+    tab.addClass('solutions-item_active');
 })
 
 $('.solutions-radio__elem[data-tab="all"]').click(function() {
     event.preventDefault();
-    $('.solutions-list__item').addClass('solutions-list__item_active');
+    $('.solutions-item').addClass('solutions-item_active');
     $(this).addClass('solutions-radio__elem_active');
 })

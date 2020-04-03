b01533ba12685e0d8dde1579edf5abce4812d274
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-04-03 19:48:45 +0300

    drop-down-anim-done

--- js/drop-down-list.js
+++ js/drop-down-list.js
@@ -1,11 +1,27 @@
 'use strict'
 
+// $('.drop-down').click(function() {
+//     if ($(this).hasClass('opened')) {
+//         $(this).next().fadeOut(300);
+//         $(this).removeClass('opened');
+//     } else {
+//         $(this).next().fadeIn(300);
+//         $(this).addClass('opened');
+//     }
+// });
+
 $('.drop-down').click(function() {
     if ($(this).hasClass('opened')) {
-        $(this).next().fadeOut(300);
+        $(this).next().animate({
+            opacity: 0,
+            height: "toggle"
+          }, 500, function() {});
         $(this).removeClass('opened');
     } else {
-        $(this).next().fadeIn(300);
+        $(this).next().animate({
+            opacity: 1,
+            height: "toggle"
+          }, 500, function() {});
         $(this).addClass('opened');
     }
 });

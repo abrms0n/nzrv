7ab30815e93a7902f583e2a72c998065b7504935
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-04-03 20:04:00 +0300

    catalog-item-margin-fix

--- js/drop-down-list.js
+++ js/drop-down-list.js
@@ -1,15 +1,5 @@
 'use strict'
 
-// $('.drop-down').click(function() {
-//     if ($(this).hasClass('opened')) {
-//         $(this).next().fadeOut(300);
-//         $(this).removeClass('opened');
-//     } else {
-//         $(this).next().fadeIn(300);
-//         $(this).addClass('opened');
-//     }
-// });
-
 $('.drop-down').click(function() {
     if ($(this).hasClass('opened')) {
         $(this).next().animate({

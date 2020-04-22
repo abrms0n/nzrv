d256995805b32579de2f5d8fef853c003fc9eddd
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-04-22 15:44:28 +0300

    show-more-fix

--- js/showMore.js
+++ js/showMore.js
@@ -1,39 +1,6 @@
 'use strict'
-if($(window).width() <= '1350') {
-    $('.hidden-text').css('display','none');
+
+function showMore(elem) {  
+    $(elem).parent().children().show();
+    $(elem).hide()
 }
-
-$('.about-gallery__text-container .show-more').click(function(){
-    $('.about-gallery__text-container .hidden-text').css('display','block');
-    $('.about-gallery__text-container .show-more').css('display','none');
-})
-
-$('.best-brands__text-container .show-more').click(function(){
-    $('.best-brands__text-container .hidden-text').css('display','block');
-    $('.best-brands__text-container .show-more').css('display','none');
-})
-
-$('.service-page-about__text .show-more').click(function(){
-    $('.service-page-about__text .hidden-text').css('display','block');
-    $('.service-page-about__text .show-more').css('display','none');
-})
-
-$('.project-manager__info .show-more').click(function(){
-    $('.project-manager__info .hidden-text').css('display','block');
-    $('.project-manager__info .show-more').css('display','none');
-})
-
-$('.listing-container__brands-container .show-more').click(function(){
-    $('.listing-container__brands-container .listing-container__brands-container_hidden').css('display','flex');
-    $('.listing-container__brands-container .show-more').css('display','none');
-})
-
-$('.product-box__about-about .show-more').click(function(){
-    $('.product-box__about-about .hidden-text').css('display','flex');
-    $('.product-box__about-about .show-more').css('display','none');
-})
-
-
-
-
-

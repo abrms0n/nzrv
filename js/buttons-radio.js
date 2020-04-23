93bf400f918cc249d1c9c36b41822ccfaed30fb3
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-04-23 14:25:03 +0300

    portfolio-item-page-1920-done

--- js/buttons-radio.js
+++ js/buttons-radio.js
@@ -3,14 +3,16 @@
 $('.buttons-radio__elem').click(function() {
     event.preventDefault();
 
-    $('.solutions-item_active').removeClass('solutions-item_active');
-    $('.buttons-radio__elem_active').removeClass('buttons-radio__elem_active');
-    let tabNo = $(this).attr('data-tab');
-    let tab = $('.solutions-item[data-tab="'+ tabNo +'"]');
+    if (!$(this).hasClass('buttons-radio__elem_disabled')) {
+        
+        $('.solutions-item_active').removeClass('solutions-item_active');
+        $('.buttons-radio__elem_active').removeClass('buttons-radio__elem_active');
+        let tabNo = $(this).attr('data-tab');
+        let tab = $('.solutions-item[data-tab="'+ tabNo +'"]');
 
-
-    $(this).addClass('buttons-radio__elem_active');
-    tab.addClass('solutions-item_active');
+        $(this).addClass('buttons-radio__elem_active');
+        tab.addClass('solutions-item_active');
+    }
 })
 
 $('.buttons-radio__elem[data-tab="all"]').click(function() {

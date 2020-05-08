94c96d477789095fe3f21cf13406a39281809018
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-05-08 16:09:59 +0300

    gulp-almost-done

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+// var uglifycss = require('gulp-uglifycss');
 
 gulp.task('sass', function () {
     gulp.src('./blocks/*.scss')
@@ -9,3 +11,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./styles'));
    });
 
+
+gulp.task('concat', function() {
+    return gulp.src('./styles/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./styles'));
+});
+
+
+/* gulp.task('css', function () {
+  gulp.src('./styles/s.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+*/

ebeda6c722bed80d70e9bd46dbb8e1c6b211c0f1
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-05-08 17:12:42 +0300

    node-modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('sass', function () {
@@ -12,20 +13,18 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./tmp'));
    });
 
-
-
+ 
 gulp.task('concat-css', function() {
     return gulp.src('./tmp/*.css')
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./styles'));
 });
 
-
- 
 gulp.task('clean', function () {
     return gulp.src('./tmp', {read: false})
         .pipe(clean());
 });
 
-
-// gulp.task('default', ['sass', 'concat-css', 'clean', ]);
+gulp.task('default', function() {  
+    gulp.run('sass', 'concat-css', 'clean');
+});

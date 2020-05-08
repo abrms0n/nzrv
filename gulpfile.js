7c088502faf5b638e934864476a7269c3877f825
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-05-08 15:54:27 +0300

    node-modules-update

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,11 @@
- // подключаем gulp
- import gulp from "gulp";
-
- // создадим две переменные, отвечающие за чтение исходных файлов (src) и запись сгенерированных файлов (dest).
- import { src, dest } from "gulp";
+'use strict';
  
- // передаем модули в переменные
- import autoprefixer from "gulp-autoprefixer";
- import rename from "gulp-rename";
- import sass from "gulp-sass";
- import cssnano from "cssnano";
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
+gulp.task('sass', function () {
+    gulp.src('./blocks/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./styles'));
+   });
 
-// создаём переменную "путь" которая будет использоваться галпом в своих методах 
-
- var path = {
-        build: {
-            // путь к новому файлу, который соберет gulp (изначально dist/)
-            css: "styles/"
-    },      
-            // путь, откуда gulp возьмёт исходные файлы (изначально src/)
-        src: {
-            css: "blocks/*.scss",
-    }
-}
-
-
-// далее сборка 
-
-function css (){
-    return src(path.src.css, {base: "blocks/"})
-        .pipe(sass())
-        .pipe(autoprefixer({
-            Browserslist: ['last 8 versions'],
-            cascade: true
-    }))
-}

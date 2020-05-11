d443ab8eac07a468e027bb34aa78d66924fb3a2b
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-05-11 14:18:20 +0300

    no-js-min-yet

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const clean = require('gulp-clean');
 const autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+
 
 
 gulp.task('sass', function () {
@@ -20,10 +22,26 @@ gulp.task('concat-css', function() {
     .pipe(gulp.dest('./styles'));
 });
 
+gulp.task('concat-js', function() {
+    return gulp.src('./src-js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('compress-js', function () {
+    return gulp.src('./tmp/*.js')
+          .pipe(uglify())
+          .pipe(gulp.dest('./js'));
+});
+
+
 gulp.task('clean', function () {
     return gulp.src('./tmp', {read: false})
         .pipe(clean());
 });
 
-gulp.task('default', gulp.series('sass','concat-css','clean'));
+gulp.task('default', gulp.series('sass','concat-css', 'concat-js', 'compress-js', 'clean'));
 
+
+
+ 
